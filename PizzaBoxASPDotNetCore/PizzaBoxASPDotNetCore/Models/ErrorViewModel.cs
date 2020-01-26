d07033095e0f09eea28504a164fea03e480e613b
John Filak <jf3163@nyu.edu>
2020-01-25 22:03:47 -0600

    MVC BoilerPlate App

---
+++ PizzaBoxASPDotNetCore/PizzaBoxASPDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaBoxASPDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
