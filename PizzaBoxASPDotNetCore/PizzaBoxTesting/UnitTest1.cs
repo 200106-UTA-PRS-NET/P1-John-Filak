8df333b13c409fb5cd6899e33839b67469c7346f
filakj <john.m.filak@gmail.com>
2020-01-31 08:48:08 -0600

    test

--- PizzaBoxASPDotNetCore/PizzaBoxTesting/UnitTest1.cs
+++ PizzaBoxASPDotNetCore/PizzaBoxTesting/UnitTest1.cs
@@ -1,12 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PizzaBoxWeb.Controllers;
 using Xunit;
 namespace PizzaBoxTesting
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
-        }
+
+        /*
+        [Fact]
+
+        
+         public void Test1()
+         {
+             PizzaUserController controller = new PizzaUserController();//new MockRepository());
+             // Act
+             var result = controller.Index() as ViewResult;
+             // Assert
+             Assert.Equal("Index", result.ViewName);
+
+         }
+
+
+     }
+     */
     }
 }
